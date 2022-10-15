e9f451799a6e41e6030cdf03ad6dc38170c0efb6
Michael Merin <michaelmerin@investagrams.com>
2022-10-15 19:48:31 +0800

    gsap application' git status

--- js/app.js
+++ js/app.js
@@ -2,31 +2,36 @@
 var app = angular.module('main', []);
 
 app.controller('MainController', function MainController($scope) {
-    let darkMode  = localStorage.getItem("dark-mode");
-        
-    function enableDarkMode() {
-        document.documentElement.style.setProperty('--primary-color', '#1A1A1A');
-        document.documentElement.style.setProperty('--off-color', '#FAFAFA');
-        localStorage.setItem("dark-mode", "enabled");
-    }
     
-    function disableDarkMode() {
-        document.documentElement.style.setProperty('--primary-color', '#FAFAFA');
-        document.documentElement.style.setProperty('--off-color', '#1A1A1A');
-        localStorage.setItem("dark-mode", "disabled");
-    }
-
-    if (darkMode === "enabled") {
-        enableDarkMode(); 
-    }
-      
-     $scope.toggleColormode = function() {
-        darkMode = localStorage.getItem("dark-mode");
-        if (darkMode === "disabled") {
-            enableDarkMode();
-        } else {
-            disableDarkMode();
+    function darkmode(){
+        let darkMode  = localStorage.getItem("dark-mode");
+        if (darkMode === "enabled") {
+            enableDarkMode(); 
         }
-    }
+        function enableDarkMode() {
+            document.documentElement.style.setProperty('--primary-color', '#1A1A1A');
+            document.documentElement.style.setProperty('--off-color', '#e5e5e5');
+            localStorage.setItem("dark-mode", "enabled");
+        }
+        
+        function disableDarkMode() {
+            document.documentElement.style.setProperty('--primary-color', '#FAFAFA');
+            document.documentElement.style.setProperty('--off-color', '#e5e5e5');
+            localStorage.setItem("dark-mode", "disabled");
+        }
+        
+        $scope.toggleColormode = function() {
+            darkMode = localStorage.getItem("dark-mode");
+            if (darkMode === "enabled") {
+                disableDarkMode();
+            } else {
+                enableDarkMode();
+            }
+        }
+    } 
     
+    function init(){
+        darkmode();
+    }
+    init();
 });
