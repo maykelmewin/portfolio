b37e45d72f6e65b3efa9b0fa21a8eaa06b8f664e
Michael Merin <michaelmerin@investagrams.com>
2022-10-17 20:52:39 +0800

    gsap parallax

--- js/pre.js
+++ js/pre.js
@@ -2,7 +2,6 @@ function calculateVh(){
     var vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', vh + 'px');
 }
-calculateVh();
 window.addEventListener('resize', calculateVh);
 window.addEventListener('orientationchange', calculateVh);
 
@@ -10,4 +9,8 @@ document.addEventListener('readystatechange', function(event) {
     if (document.readyState === "complete") {
         document.querySelector('.loading-screen').classList.add('--closed');
     }
+    
+    calculateVh();
+    pageReadyAnimation();
+    
 });
