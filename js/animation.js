91e5ed8a669931f3c5e62a6ea3227d9eda1a85eb
Michael Merin <michaelmerin@investagrams.com>
2023-03-30 16:53:28 +0800

    update

--- js/animation.js
+++ js/animation.js
@@ -1,11 +1,11 @@
 gsap.registerPlugin(ScrollTrigger);
 
+// second page slides
 const  slides = gsap.to(".slides", {
     xPercent: -200,
     ease: "none",
     duration: .2
 });
-
 ScrollTrigger.create({
     animation: slides,
     trigger: ".fold-second",
@@ -16,13 +16,28 @@ ScrollTrigger.create({
     end: () => "+=" + document.querySelector(".slides").offsetWidth
 });
 
+// last page trigger
 ScrollTrigger.create({
     trigger: ".fold-last",
     start: "top bottom",
     toggleClass: {targets: ".infocard", className: "--expand"},
 })
+// main button
+document.querySelector("#mainBtn").addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: {y:".fold-first-content"} })
+});
 
+var pageDown = document.querySelectorAll(".--pagedown")
+pageDown.forEach(function (el){  
+    el.addEventListener("click", () => {
+        gsap.to(window, {duration: 1, scrollTo: {y:".fold-last"} })
+    });
+});
 
+document.querySelector(".--pageup").addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo: {y:".fold-first"} })
+});
+// clouds parallax
 document.querySelectorAll(".pagination__item").forEach((btn, index, array) => {
     btn.addEventListener("click", () => {
     gsap.to(window, {duration: 1, scrollTo:{y:"#phase" + (index + 1)}});
@@ -35,7 +50,6 @@ document.querySelectorAll(".pagination__item").forEach((btn, index, array) => {
         toggleClass: {targets: '#btnPhase' + (index + 1), className: '--active'}
     });
 });
-
 function pageReadyAnimation(){
    const falling = gsap.timeline();
     falling.fromTo('.--a0', {yPercent: -1000}, {yPercent: 1000, duration: '2'})
@@ -53,8 +67,17 @@ function pageReadyAnimation(){
         end: () => "+=" + document.querySelector(".fold-third").offsetWidth,
         scrub: true,
     });
-
 }
+// toggle aninamation
+const switching = gsap.timeline();
+switching.to('.toggle', {rotation:180, duration: '1'})
+.to('.toggle__button', {y:20, duration: '1'}, "<")
+switching.reversed(true);
+function toggleAnimation(){
+    switching.reversed(!switching.reversed());
+}
+
+// me falling animation
 gsap.to(".me.--falling", {
     scrollTrigger: {
         trigger: ".fold-third",
@@ -64,29 +87,24 @@ gsap.to(".me.--falling", {
     top: "300%"
 });
 
-
-var cursor = document.querySelector(".cursor");
+// customize mouse pointer
+var cursor = document.querySelector(".cursor"),
     follower =  document.querySelector(".cursor-follower");
-
 var posX = 0,
     posY = 0;
-
 var mouseX = 0,
     mouseY = 0;
-
 TweenMax.to({}, 0.016, {
   repeat: -1,
   onRepeat: function() {
     posX += (mouseX - posX) / 9;
     posY += (mouseY - posY) / 9;
-    
     TweenMax.set(follower, {
         css: {    
         left: posX - 21,
         top: posY - 21
         }
     });
-    
     TweenMax.set(cursor, {
         css: {    
         left: mouseX,
@@ -111,3 +129,65 @@ document.querySelectorAll(".focusmouse").forEach((item) => {
         follower.classList.remove("active");
     });
 });
+
+
+//pinning me homepage
+gsap.to(".me.--main", {
+    scrollTrigger: {
+        trigger: ".me.--main",
+        toggleActions: "restart none none reverse",
+        pin: true,
+        start: "top 20%",
+        end: () => (window.innerHeight - (window.innerHeight * .1)) + " 20%"
+    },
+});
+//pinning me homepage
+gsap.to(".hero-content__name", {
+    scrollTrigger: {
+        trigger: ".hero-content",
+        start: "top top",
+        scrub: true
+    },
+    scale: 10,
+    yPercent: -1000
+});
+
+// me animations
+var headRestX = '-50%',
+headRestY = 0,
+bodyRestX = '-50%',
+bodyRestY = 0,
+rhandRestX = 0 ,
+rhandRestY = 0,
+lhandRestX = 0,
+lhandRestY = 0;
+
+// salute
+tlSalute =  gsap.timeline({ paused: true});
+tlSalute.fromTo('.me__part.--head', {x: headRestX, y: headRestY}, {x: headRestX, y: headRestY , duration: '1'})
+.fromTo('.me__part.--body' , {x: bodyRestX, y: bodyRestY}, {x: bodyRestX, y: bodyRestY , duration: '1'}, "<")
+.fromTo('.me__part.--rhand', {x: rhandRestX, y: rhandRestY} ,{x: -12, y: rhandRestY, duration: '1'}, "<")
+.fromTo('.me__part.--lhand', {x: lhandRestX, y: lhandRestY}  ,{x: 46, y: -38 , duration: '1'}, "<");
+
+// handsup
+tlHandsup = gsap.timeline({ paused: true});
+tlHandsup.fromTo('.me__part.--head', {x: headRestX, y: headRestY} ,{x: headRestX, y: -10, duration: '1'})
+.fromTo('.me__part.--body', {x: bodyRestX, y: bodyRestY} ,{x: bodyRestX, y: bodyRestY , duration: '1'}, "<")
+.fromTo('.me__part.--rhand', {x: rhandRestX, y: rhandRestY} ,{x: 10, y: -160 , duration: '1'}, "<")
+.fromTo('.me__part.--lhand', {x: lhandRestX, y: lhandRestY} ,{x: -10, y: -160 , duration: '1'}, "<");
+
+// rest
+tlRest = gsap.timeline({ paused: true});
+tlRest.to('.me__part.--head' ,{x: headRestX, y: headRestY, duration: '1'})
+.to('.me__part.--body' ,{x: bodyRestX, y: bodyRestY , duration: '1'}, "<")
+.to('.me__part.--lhand' ,{x: lhandRestX, y: lhandRestY , duration: '1'}, "<")
+.to('.me__part.--rhand' ,{x: rhandRestX, y: rhandRestY , duration: '1'}, "<");
+
+// wave
+tlWave = gsap.timeline({ paused: true});
+tlWave.fromTo('.me__part.--head', {x: headRestX, y: headRestY,} ,{x: headRestX, y: headRestY, duration: '1'})
+.fromTo('.me__part.--body', {x: bodyRestX, y: bodyRestY} ,{x: bodyRestX, y: bodyRestY , duration: '1'}, "<")
+.fromTo('.me__part.--rhand', {x: lhandRestX, y: lhandRestY},{x: 60, y: -80}, "<" )
+.to('.me__part.--rhand', {x: -20, y: -160, duration: '1',repeat: -1, yoyo: true}, "<" )
+.fromTo('.me__part.--lhand', {x: lhandRestX, y: lhandRestY} ,{x: lhandRestX, y: lhandRestY , duration: '1'}, "<");
+// tlWave.play();
--- js/app.js
+++ js/app.js
@@ -45,11 +45,65 @@ app.controller('MainController', function MainController($scope) {
        
     ]
 
+    $scope.expertise = [
+        {
+            skills: 'Responsive',
+            info: 'Build a fully responsive cross-device/cross-browser and pixel-perfect HTML prototype based on the visual mock-up.'
+        },
+        {
+            skills: 'Frameworks and Libraries',
+            info: 'Implement the desired front-end frameworks and libraries efficiently.'
+        },
+        {
+            skills: 'API',
+            info: 'Effectively manage, integrate, and modify APIs.'
+        }
+    ]
+
     $scope.animate = 0;
     $scope.animateMe = function(id) {
-        // alert(id);
+        
+        var meAnim = { 
+            'play': [ 
+                function(){
+                    tlRest.play();
+                },
+                function(){
+                    tlSalute.play();
+                },
+                function(){
+                    tlWave.play(1);
+                },
+                function(){
+                    tlHandsup.play();
+                },
+            ],
+            'reverse': [
+                function(){
+                    tlRest.reverse();
+                },
+                function(){
+                   tlSalute.reverse();
+                },
+                function(){    
+                    tlWave.reverse();
+                    tlWave.pause();            
+                },
+                function(){
+                  tlHandsup.reverse();                  
+                },
+            ]
+        };
+        meAnim.reverse[$scope.animate]();
+        clearTimeout(setTimeout(meAnim.play[$scope.animate], 1000));
+        setTimeout(meAnim.play[id], 1000);
         $scope.animate = id;
     }
+    $scope.loadModel = function() {
+        document.querySelector('#modelBox').classList.add('window--close')
+        document.querySelector('#lazy-load').dismissPoster()
+    };
+    
     function darkmode(){
         let darkMode  = localStorage.getItem("dark-mode");
         if (darkMode === "enabled") {
@@ -58,17 +112,20 @@ app.controller('MainController', function MainController($scope) {
         function enableDarkMode() {
             document.documentElement.style.setProperty('--primary-color', '#1A1A1A');
             document.documentElement.style.setProperty('--off-color', '#e5e5e5');
+            document.getElementsByClassName('infocard')[0].classList.add('--inverted');
             localStorage.setItem("dark-mode", "enabled");
         }
         
         function disableDarkMode() {
             document.documentElement.style.setProperty('--primary-color', '#e5e5e5');
             document.documentElement.style.setProperty('--off-color', '#1A1A1A');
+            document.getElementsByClassName('infocard')[0].classList.remove('--inverted');
             localStorage.setItem("dark-mode", "disabled");
         }
         
         $scope.toggleColormode = function() {
             darkMode = localStorage.getItem("dark-mode");
+            toggleAnimation();
             if (darkMode === "enabled") {
                 disableDarkMode();
             } else {
